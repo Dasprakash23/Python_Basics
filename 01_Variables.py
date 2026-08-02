81599a5f2e66f9cc879e421268d4f0e30ca97192
Dasprakash .M <dasprakashmanikandan19@gmail.com>
2026-08-02 21:28:52 +0530

    Variables

---
+++ 01_Variables.py
@@ -0,0 +1,16 @@
+# Store personal details
+name = "Dasprakash"
+age = 21
+cgpa = 8.4
+
+# Display personal details
+print("My name is", name)
+print("My age is", age)
+print("My CGPA is", cgpa)
+
+# Get user details
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+# Display greeting message
+print("Hello", user_name, "you are", user_age, "years old.")
