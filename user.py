6836f8f03ccfe000a233692131db57313f39f439
Dasprakash .M <dasprakashmanikandan19@gmail.com>
2026-08-05 07:23:05 +0530

    getiing info from the users

---
+++ user.py
@@ -0,0 +1,18 @@
+# ------------------------------------
+# Python User Input Example
+# ------------------------------------
+
+# Taking string input
+name = input("Enter your name: ")
+
+# Taking integer input
+age = int(input("Enter your age: "))
+
+# Taking float input
+height = float(input("Enter your height in meters: "))
+
+# Displaying the values
+print("\n----- User Details -----")
+print("Name :", name)
+print("Age :", age)
+print("Height :", height, "meters")
