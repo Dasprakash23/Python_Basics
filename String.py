4f771075edf8416b138f6375147ac5051d014931
Dasprakash .M <dasprakashmanikandan19@gmail.com>
2026-08-05 07:24:45 +0530

    Explained all string operations

---
+++ String.py
@@ -0,0 +1,35 @@
+# ------------------------------------
+# Python Strings Example
+# ------------------------------------
+
+text = "Hello Python"
+
+print("Original String :", text)
+
+# Length
+print("Length :", len(text))
+
+# Uppercase
+print("Uppercase :", text.upper())
+
+# Lowercase
+print("Lowercase :", text.lower())
+
+# Replace
+print("Replace :", text.replace("Python", "World"))
+
+# First character
+print("First Character :", text[0])
+
+# Last character
+print("Last Character :", text[-1])
+
+# Slicing
+print("First Five Characters :", text[:5])
+
+# Concatenation
+name = "Leo"
+print("Welcome " + name)
+
+# Repetition
+print(name * 3)
