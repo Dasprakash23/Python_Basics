6847c15ea24a28afbf639bfbfc2354795015a2da
Dasprakash .M <dasprakashmanikandan19@gmail.com>
2026-08-05 07:41:53 +0530

    Bool operations

---
+++ Boolean.py
@@ -0,0 +1,28 @@
+# ------------------------------------
+# Python Boolean Example
+# ------------------------------------
+
+a = 20
+b = 10
+
+print("a =", a)
+print("b =", b)
+
+# Comparison results
+print("a > b :", a > b)
+print("a < b :", a < b)
+print("a == b :", a == b)
+print("a != b :", a != b)
+
+# Boolean variables
+is_student = True
+is_employee = False
+
+print("\nStudent :", is_student)
+print("Employee :", is_employee)
+
+# Using bool()
+print("\nBoolean of 1 :", bool(1))
+print("Boolean of 0 :", bool(0))
+print("Boolean of 'Python' :", bool("Python"))
+print("Boolean of '' :", bool(""))
